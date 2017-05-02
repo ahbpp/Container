aa467fc1df3e93922588995d32468c6009053946
Alexey <ahbpp@hotmail.com>
2017-05-02 14:20:46 +0300

    base

---
+++ container/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  container
+//
+//  Created by Alexey Karpov on 02.05.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
